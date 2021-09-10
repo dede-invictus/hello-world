0c4affa24c361245d30bfbb6f62e6b1ff83c7520
Dede <dede@invictus.school>
2021-09-10 23:56:54 +0630

    first commit

---
+++ js/learning_git.js
@@ -0,0 +1,12 @@
+let dede; // variable declaration
+dede = 'Dede is so stupid'; // value assign to the variable 
+
+let give_me_a_glass_of_water  = 'A glass of water'; // Snake Case
+let giveMeAGlassOfWater  = 'A glass of water'; // Camel Case
+// ABC Uppercase 
+// abc Lowercase 
+
+let singleQuoteString = 'Hey, I am a SingleQuote string'; // String 
+let dobuleQuoteString = "Hey, I am a DoubleQuote string"; // String 
+let pyramidHeight = 1233; // Number 
+let pyramidLength = 2000.23423; // Float/Decimal
---
+++ js/vim_tutorial.js
@@ -0,0 +1,1 @@
+let myVim = 'I am vim';
